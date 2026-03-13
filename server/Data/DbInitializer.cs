5540156ae74334b4589b91c37fd3d21b78e53aa4
Huy Phan <huy.phan2210@outlook.com>
2026-03-13 13:23:49 +0700

    [FEAT - SignUp] [server] Change Seed Data for User

--- server/Data/DbInitializer.cs
+++ server/Data/DbInitializer.cs
@@ -20,14 +20,14 @@ namespace server.Data
       using var scope = serviceProvider.CreateScope();
       var userManager = scope.ServiceProvider.GetRequiredService<UserManager<User>>();
 
-      var guest = await userManager.FindByNameAsync("guest");
+      var guest = await userManager.FindByNameAsync("guest@example.com");
 
       if (guest == null)
       {
         var user = new User
         {
           Id = Guid.Parse("019cd5ad-2246-765d-8d3b-27610e81c6ca"),
-          UserName = "guest",
+          UserName = "guest@example.com",
           Email = "guest@example.com",
           EmailConfirmed = true,
           FullName = "Guest User"
