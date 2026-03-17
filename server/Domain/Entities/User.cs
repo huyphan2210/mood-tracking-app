d709eb7fd00fcee8d0da0e83c0a45a323435e84f
Huy Phan <huy.phan2210@outlook.com>
2026-03-17 13:18:58 +0700

    [FEAT - Onboarding] [server] Add AvatarURL to Entity User

--- server/Domain/Entities/User.cs
+++ server/Domain/Entities/User.cs
@@ -5,6 +5,7 @@ namespace server.Domain.Entities
   public class User : IdentityUser<Guid>
   {
     public string? FullName { get; set; }
+    public string? AvatarURL { get; set; }
     public DateTime CreatedAt { get; protected set; } = DateTime.UtcNow;
     public DateTime? UpdatedAt { get; protected set; }
     public Boolean IsDeleted { get; set; }
---
+++ server/Migrations/20260317030122_User_AddAvatarURL.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server.Migrations
+{
+    /// <inheritdoc />
+    public partial class User_AddAvatarURL : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "avatar_url",
+                table: "AspNetUsers",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "avatar_url",
+                table: "AspNetUsers");
+        }
+    }
+}
