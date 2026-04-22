d133b40327429e64585cf79b8aa3f6e09b13bc93
Huy Phan <huy.phan2210@outlook.com>
2026-04-22 10:21:48 +0700

    [FEAT - Home Page] [client] Add icons, lib and default styles

--- client/src/lib/date.ts
+++ client/src/lib/date.ts
@@ -23,3 +23,34 @@ export const formatToday = () => {
 
   return `${weekday}, ${month} ${day}${getOrdinal(day)}, ${year}`;
 };
+
+export const formatDateToMonthAndYear = (date: Date) => {
+  const month = date.toLocaleDateString("en-US", { month: "long" });
+  const year = date.getFullYear();
+
+  return `${month} | ${year}`;
+};
+
+export const MONTHS_RECORDS: Record<number, string> = {
+  0: "January",
+  1: "Febuary",
+  2: "March",
+  3: "April",
+  4: "May",
+  5: "June",
+  6: "July",
+  7: "August",
+  8: "Setemper",
+  9: "October",
+  10: "November",
+  11: "December",
+};
+
+export const MONTH_OPTIONS = Object.keys(MONTHS_RECORDS).map((key) => ({
+  label: MONTHS_RECORDS[parseInt(key)],
+  value: parseInt(key),
+}));
+
+export const YEAR_OPTIONS = [
+  2026, 2027, 2028, 2029, 2030, 2031, 2032, 2033, 2034, 2035, 2036, 2037,
+];
